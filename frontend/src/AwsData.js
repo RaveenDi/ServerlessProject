4504765905ffac6a8f952e05186904a4dc7e13db
KoshanaThenuwara <154039264+KoshanaThenuwara@users.noreply.github.com>
2023-12-31 20:20:20 +0530

    Update AwsData.js add aws data

--- frontend/src/AwsData.js
+++ frontend/src/AwsData.js
@@ -1,5 +1,5 @@
 export const awsData = {
-  REGION: "region",
-  USER_POOL_ID: "",
-  USER_POOL_APP_CLIENT_ID: "",
+  REGION: "eu-north-1",
+  USER_POOL_ID: "eu-north-1_dueh78bFU",
+  USER_POOL_APP_CLIENT_ID: "3v6oqhb488t8quk88h35sg0fs8",
 };
